7bd001846f2a770df2b077d06379a751cd993aa8
chandrakanthrck <chandrakanthrajesh@Chandrakanths-MacBook-Pro-2.local>
2024-09-25 14:30:56 -0700

    Spring Initializr setup

---
+++ twilio-communication/src/main/java/com/chandrakanthrck/twilio_communication/TwilioCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.chandrakanthrck.twilio_communication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioCommunicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioCommunicationApplication.class, args);
+	}
+
+}
